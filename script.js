3acb04983c7aea0997f60b3a8e888a28606b2754
Nick VanMeter <nickjvm@gmail.com>
2025-01-25 16:37:46 -0700

    add meal photos

--- script.js
+++ script.js
@@ -22,57 +22,102 @@ function getCookie(name) {
 }
 
 
-(function () {
+(async function () {
     const week = last7Days(1)
     const token = getCookie('tz_uatoken')
     const uid = getCookie('tz_uid')
+
     if (!token || !uid) {
         return alert('Please sign in again.')
+    }
+
+    function getPhoto(photoId, mealId, size = "full") {
+        return fetch(`https://api.trainerize.com/v03/dailyNutrition/getMealPhoto?mealPhotoID=${photoId}&mealGuid=${mealId}&size=${size}`, {
+            headers: {
+                Authorization: `Bearer ${getCookie('tz_uatoken')}`
+            }
+        }).then(r => r.blob()).then(r => URL.createObjectURL(r)).catch(e => {
+            console.log('error fetching image')
+        })
+    }
+
+    function getPhotos() {
+        const photos = document.querySelectorAll('.meal--image[data-photo-id]')
+
+        const requests = Array.from(photos).map(container => {
+            return getPhoto(container.dataset.photoId, container.dataset.mealId).then(r => {
+                delete container.dataset.photoId
+                delete container.dataset.mealId
+
+                const img = document.createElement('img')
+                img.src = r
+                img.alt = 'meal photo'
+
+                container.insertAdjacentElement('afterbegin', img)
+            })
+        })
+
+        return Promise.allSettled(requests)
     }
 
     function macroGoal(macro) {
         return {
             label: `${Math.round((macro.grams / macro.goal) * 100)}%`,
             average: `${Math.round(macro.grams / week.length)}g`,
-            percentage: (macro.grams / macro.goal) * 100 ,
+            percentage: (macro.grams / macro.goal) * 100,
         }
     }
 
-    const requests = week.map(date => fetch('https://api.trainerize.com/v03/dailyNutrition/get', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // Datetoday: '2025-01-21 09:26:33',
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({
-            date,
-            userid: uid
+
+    function getSettings() {
+        return fetch('https://api.trainerize.com/v03/user/getSettings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ userid: uid })
+        }).then(r => r.json()).catch(e => { })
+    }
+
+    try {
+
+        const requests = week.map(date => fetch('https://api.trainerize.com/v03/dailyNutrition/get', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Datetoday: '2025-01-21 09:26:33',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                date,
+                userid: uid
+            })
         })
-    })
-        .then(response => {
-            if (!response.ok) {
-                switch (response.status) {
-                    // Check if the response is not OK (status code not in the range 200-299)
-                    case 401: {
-                        throw new Error('Please sign in again.')
-                    }
-                    default: {
-                        throw new Error('Something went wrong. Please try again.')
+            .then(response => {
+                if (!response.ok) {
+                    switch (response.status) {
+                        // Check if the response is not OK (status code not in the range 200-299)
+                        case 401: {
+                            throw new Error('Please sign in again.')
+                        }
+                        default: {
+                            throw new Error('Something went wrong. Please try again.')
+                        }
                     }
                 }
+
+                return response.json(); // Assuming the response is JSON
+            })
+            .catch(e => {
+                alert(e.message)
             }
-
-            return response.json(); // Assuming the response is JSON
-        })
-        .catch(e => {
-            alert(e.message)
-        }))
-
-    Promise.all(requests).then((logs) => {
-        const macros = logs.reduce((acc, data) => {
+            ))
+        const logs = await Promise.allSettled(requests)
+        const settings = await getSettings()
+        const macros = logs.reduce((acc, { value: data }) => {
             acc.calories += data.nutrition.calories
-            acc.caloricGoal += data.nutrition.goal.caloricGoal 
+            acc.caloricGoal += data.nutrition.goal.caloricGoal
             acc.protein.grams += data.nutrition.proteinGrams
             acc.protein.percent += data.nutrition.proteinPercent
             acc.protein.goal += data.nutrition.goal.proteinGrams
@@ -85,7 +130,7 @@ function getCookie(name) {
             acc.fiber += data.nutrition.nutrients.find(n => n.nutrNo === 291)?.nutrVal
             return acc
         }, {
-            calories: 0, 
+            calories: 0,
             caloricGoal: 0,
             protein: {
                 goal: 0,
@@ -107,7 +152,7 @@ function getCookie(name) {
 
         const summary = `
             <div class="section">
-                <h1>Weekly Meal Report (${formatDate(week[0])} - ${formatDate(week[week.length - 1])})</h1>
+                <h1>${settings.stats.firstName} ${settings.stats.lastName}'s Meal Report (${formatDate(week[0])} - ${formatDate(week[week.length - 1])})</h1>
                 <h2>Macro distribution</h2>
                 <div class="bar-container">
                     <div class="bar">
@@ -156,17 +201,17 @@ function getCookie(name) {
         document.body.insertAdjacentHTML('afterbegin', '<div id="custom-trainerize-export"></div>')
         document.querySelector('#custom-trainerize-export').insertAdjacentHTML('afterbegin', summary)
 
-        logs.map(data => {
+        logs.map(({ value: data }) => {
             const entryTemplate = `
                 <div class="pagebreak"></div>
                 <section class="daily-entry">
                     <header>
                         <h2>${formatDate(data.nutrition.date, {
-                            weekday: "short",
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                        })}</h2>
+                weekday: "short",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+            })}</h2>
                     </header>
                     <div class="bar-container">
                         <div class="bar">
@@ -200,60 +245,71 @@ function getCookie(name) {
                     </div>
                     ${data.nutrition.meals.map(meal => `
                         <div class="meal">
-                            <h3>
-                                <span class="meal--timestamp">${new Date(meal.mealTime).toLocaleString('en-US', { hour: '2-digit', hour12: true, minute: '2-digit' })}</span>
-                                <span class="meal--type">${meal.name}</span>
-                                <span class="meal--calories">${Math.round(meal.caloriesSummary)} Calories</span>
-                            </h3>
-                            <div class="bar-container">
-                                <div class="bar">
-                                    <div class="protein" style="width: ${meal.proteinPercent}%"></div>
-                                    <div class="carbs" style="width: ${meal.carbsPercent}%"></div>
-                                    <div class="fat" style="width: ${meal.fatPercent}%"></div>
+                            ${meal.hasImage ? `<div class="meal--image" data-photo-id="${data.nutrition.mealPhoto.id}" data-meal-id="${meal.mealGuid}"></div>` : ''}
+                            <div class="meal--details">
+                                <h3>
+                                    <span class="meal--timestamp">${new Date(meal.mealTime).toLocaleString('en-US', { hour: '2-digit', hour12: true, minute: '2-digit' })}</span>
+                                    <span class="meal--type">${meal.name}</span>
+                                    <span class="meal--calories">${Math.round(meal.caloriesSummary)} Calories</span>
+                                </h3>
+                                <div class="bar-container">
+                                    <div class="bar">
+                                        <div class="protein" style="width: ${meal.proteinPercent}%"></div>
+                                        <div class="carbs" style="width: ${meal.carbsPercent}%"></div>
+                                        <div class="fat" style="width: ${meal.fatPercent}%"></div>
+                                    </div>
+                                    <div class="legend">
+                                        <div class="label">
+                                            <div class="icon protein"></div>Protein ${Math.round(meal.proteinSummary)} (${Math.round(meal.proteinPercent)}%)
+                                        </div>
+                                        <div class="label">
+                                            <div class="icon carbs"></div>Carbs ${Math.round(meal.carbsSummary)} (${Math.round(meal.carbsPercent)}%)
+                                        </div>
+                                        <div class="label">
+                                            <div class="icon fat"></div>Fat ${Math.round(meal.fatSummary)} (${Math.round(meal.fatPercent)}%)
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="legend">
-                                    <div class="label">
-                                        <div class="icon protein"></div>Protein ${Math.round(meal.proteinSummary)} (${Math.round(meal.proteinPercent)}%)
-                                    </div>
-                                    <div class="label">
-                                        <div class="icon carbs"></div>Carbs ${Math.round(meal.carbsSummary)} (${Math.round(meal.carbsPercent)}%)
-                                    </div>
-                                    <div class="label">
-                                        <div class="icon fat"></div>Fat ${Math.round(meal.fatSummary)} (${Math.round(meal.fatPercent)}%)
-                                    </div>
-                                </div>
+                                <ul class="meal-items">
+                                    ${meal.foods.map(food => `
+                                        <li class="meal-item">
+                                            <div>
+                                                <p class="meal-item--name">${food.name}</p>
+                                                <p class="meal-item--size">${food.amount} ${food.unit}</p>
+                                            </div>
+                                            <div class="meal-item--calories">
+                                            ${Math.round(food.calories)} cal
+                                            </div>
+                                        </li>
+                                    `).join('')}
+                                </ul>
                             </div>
-                            <ul class="meal-items">
-                                ${meal.foods.map(food => `
-                                    <li class="meal-item">
-                                        <div>
-                                            <p class="meal-item--name">${food.name}</p>
-                                            <p class="meal-item--size">${food.amount} ${food.unit}</p>
-                                        </div>
-                                        <div class="meal-item--calories">
-                                        ${Math.round(food.calories)} cal
-                                        </div>
-                                    </li>
-                                `).join('')}
-                            </ul>
                         </div>
                     `).join('')}
                 </section>
             `
             document.body.querySelector('#custom-trainerize-export').insertAdjacentHTML('beforeend', entryTemplate)
         })
-        // window.print()
 
-    })
+        await getPhotos()
 
-    window.onafterprint = async function () {
+        window.onafterprint = async function () {
 
-        // Message sender
-        await chrome.runtime.sendMessage({
-            status: 'complete'
-        });
-        // console.log("Print dialog closed or printing finished.");
-        // document.querySelector('#custom-trainerize-export')?.remove()
-        // document.body.classList.remove('custom-trainerize-export-active')
-    };
+            // Message sender
+            await chrome.runtime.sendMessage({
+                status: 'complete'
+            });
+            // console.log("Print dialog closed or printing finished.");
+            // document.querySelector('#custom-trainerize-export')?.remove()
+            // document.body.classList.remove('custom-trainerize-export-active')
+        };
+
+        window.print()
+
+    } catch (e) {
+        console.log(e)
+
+        document.body.classList.remove('custom-trainerize-export-active')
+        document.querySelector('#custom-trainerize-export')?.remove()
+    }
 })()
