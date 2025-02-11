a6dc024d97ff6f4fd3c4ee604195538258bfa99a
Nick VanMeter <nickjvm@gmail.com>
2025-02-11 13:18:22 -0700

    fix error handling on fetch request errors

--- src/components/DiaryContext.tsx
+++ src/components/DiaryContext.tsx
@@ -17,7 +17,7 @@ type Props = {
 }
 
 export function DiaryContextProvider({ children }: Props) {
-    const token = getCookie("tz_uatoken");
+    const token = getCookie("tz_uatoken") || getCookie('tr_uatoken');
     const uid = getCookie("tz_uid");
     const week = last7Days(1);
 
@@ -27,27 +27,38 @@ export function DiaryContextProvider({ children }: Props) {
 
     useEffect(() => {
         startTransition(async () => {
-            const settings = await getSettings();
-            const requests = week.map(getDailyNutrition);
+            try {
+                const settings = await getSettings();
+                const requests = week.map(getDailyNutrition);
 
-            const logs = await Promise.allSettled<Nutrition>(requests);
+                const logs = await Promise.allSettled<Nutrition>(requests);
 
-            if (logs.some(l => l.status === 'rejected' && l.reason.status === 401)) {
-                alert('Please sign in again.');
-                window.location.href = `${window.location.origin}/app/login`;
+                if (logs.some(l => l.status === 'rejected' && l.reason.status === 401)) {
+                    alert('Please sign in again.');
+                    window.location.href = `${window.location.origin}/app/login`;
+                    return;
+                }
+
+                const data = logs.map(result => {
+                    if (result.status === 'fulfilled') {
+                        return result.value;
+                    } else {
+                        return null;
+                    }
+                });
+
+                setSettings(settings);
+                setData(data);
+            } catch (e) {
+                if ((e as ErrorWithStatus).status === 401) {
+                    alert('Please sign in again.');
+                    window.location.href = `${window.location.origin}/app/login`;
+                } else {
+                    alert('Something went wrong. Please try again.');
+                    document.querySelector('.tpe-print-button')?.removeAttribute('disabled');
+                }
                 return;
             }
-
-            const data = logs.map(result => {
-                if (result.status === 'fulfilled') {
-                    return result.value;
-                } else {
-                    return null;
-                }
-            });
-
-            setSettings(settings);
-            setData(data);
         });
 
     }, []);
